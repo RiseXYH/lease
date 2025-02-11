7277919d8f15cdac75c9830330ea4e945deb2c60
DESKTOP-OG6HSA3\少禹 <2681762612@qq.com>
2025-02-11 17:19:29 +0800

    定时任务

--- web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -2,8 +2,11 @@ package com.atguigu.lease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+// 开启定时任务
+@EnableScheduling
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
