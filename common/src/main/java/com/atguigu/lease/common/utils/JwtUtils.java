b1c800f635a40f19cccc82d2f9c483482565b87d
XYH <2681762612@qq.com>
2025-02-13 20:50:25 +0800

    登录功能

--- common/src/main/java/com/atguigu/lease/common/utils/JwtUtils.java
+++ common/src/main/java/com/atguigu/lease/common/utils/JwtUtils.java
@@ -1,7 +1,8 @@
 package com.atguigu.lease.common.utils;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
@@ -12,11 +13,11 @@ public class JwtUtils {
     public static String createToken(Long userId, String username){
 
         String Jwt = Jwts.builder().
-//  设置过期时间
+    //  设置过期时间
         setExpiration(new Date(System.currentTimeMillis() + 3600000)).
                 setSubject("LOGIN_USER").
                 claim("userId", userId).
-//  claim声明自己定义的方法
+    //  claim声明自己定义的方法
         claim("username", username).
                 //设置签名算法 HS系列
                         signWith(secretKey, SignatureAlgorithm.HS256).
@@ -25,10 +26,25 @@ public class JwtUtils {
 
         
     }
+    //就是读取token，查有没有这个用户？然后要注意是否过期？因为return没填写jwt
+    public static Claims parseToken(String token){
+
+        if (token==null){
+            throw new LeaseException(ResultCodeEnum.ADMIN_LOGIN_AUTH);
+        }
+
+        try{
+            JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(secretKey).build();
+            return jwtParser.parseClaimsJws(token).getBody();
+        }catch (ExpiredJwtException e){
+            throw new LeaseException(ResultCodeEnum.TOKEN_EXPIRED);
+        }catch (JwtException e){
+            throw new LeaseException(ResultCodeEnum.TOKEN_INVALID);
+        }
+    }
 
 
-
-//    测试方法
+    //测试方法
     public static void main(String[] args) {
         String token = createToken(2L, "zhangsan");
         System.out.println(token);
